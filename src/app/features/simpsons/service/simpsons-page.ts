40db205ed46bab45c7121f659f0db393bb1ca809
Milton082 <chuquimilton53@gmail.com>
2026-05-26 21:19:45 -0500

    feat: registrar ruta simpsons/:id en app.routes

--- src/app/features/simpsons/service/simpsons-page.ts
+++ src/app/features/simpsons/service/simpsons-page.ts
@@ -2,11 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable, catchError, delay, map, tap, throwError, timeout } from 'rxjs';
 import { Options, SimpsonsCharacter, SimpsonsResponse } from '../models/simpsons.interface';
+import { environment } from '../../../../environments/environment.development';
 
 @Injectable({ providedIn: 'root' })
 export class SimpsonsService {
   private http = inject(HttpClient);
-  private readonly baseUrl = 'https://thesimpsonsapi.com/api';
+  private readonly baseUrl = environment.apiUrl;
   // private readonly baseUrl = environment.apiUrl;
 
   getCharactersOptions(options: Options = {}): Observable<SimpsonsResponse> {
